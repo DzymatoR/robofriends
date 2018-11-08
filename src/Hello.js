20c9a2bc4e7c3511b0853f224a51176e83a3c55b
DzymatoR <dzymator@gmail.com>
2018-11-08 22:22:00 +0100

    další lekce

---
+++ src/Hello.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class Hello extends Component {
+render(){
+    return (
+    <div className = "f1 tc">
+        <h1> Hello world </h1>
+        <p> whats going on</p>
+
+    </div>
+    
+    )
+}
+
+
+}
+
+export default Hello;
